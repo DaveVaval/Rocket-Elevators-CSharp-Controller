7d9d6f9daf128a85424302be155e9734c3da16a2
DaveGrim <DaveGrim@users.noreply.github.com>
2021-02-08 11:34:53 -0500

    Learning about classes in C#

--- commercial_controller.cs
+++ commercial_controller.cs
@@ -3,11 +3,32 @@ using System.Collections.Generic;
 
 namespace C_
 {
+    public class Battery
+    {
+        public int ID;
+        public string status;
+        public int amountOfFloors;
+        public string[] columnList;
+
+        // public void createColumns()
+        // {
+        //     for (int i = 0; i < this.amountOfFloors; i++){
+        //         columnList[i] = new Column();
+        //     }
+        // }
+    }
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Battery test = new Battery();
+            test.ID = 1;
+            Console.WriteLine(test.ID);
         }
+
     }
 }
+
+
